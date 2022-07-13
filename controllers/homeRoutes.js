ca3b123d531dc41d9afbff5cbf76763383513cc4
mohamed hassan <mohamedisaaq0@gmail.com>
2022-07-13 17:15:07 +0100

    importing api routes into index.js file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,4 +4,17 @@ router.get('/', async (req, res) => {
   res.render('homepage');
 });
 
+router.get('/login', async (req, res) => {
+  res.send(`<h1>this is the login page</h1>`);
+  res.render('login');
+});
+
+router.get('/language', async (req, res) => {
+  res.send(`<h1>this is the language page</h1>`);
+});
+
+router.get('/user/:id', async (req, res) => {
+  res.send(`<h1>this is the my account</h1>`);
+});
+
 module.exports = router;
