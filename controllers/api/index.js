3316d6cee1d05d653ad6861d4254ccc3b67b9182
mohamed hassan <mohamedisaaq0@gmail.com>
2022-07-14 19:05:02 +0100

    imported all the routes in the index file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 
-router.use('/users', userRoutes);
+router.use('/user', userRoutes);
+router.use('/language', languageRoutes);
+router.use('/module', moduleRoutes);
 
 module.exports = router;
