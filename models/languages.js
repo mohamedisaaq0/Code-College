888c18dcb02cc476764bd7b496f2d0b7567d4a50
Aisha Abdi <aishahajisirad@gmail.com>
2022-07-14 14:53:34 +0100

    Created models

---
+++ models/languages.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Languages extends Model {}
+Languages.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  UserId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
---
+++ models/model.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+class modules extends Model {
+}
+
+modules.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  progress: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    unique: true,
+  },
+
+  languageId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
