ca15f01d48d5d1a7c3568656b0aaca251cf1171f
mohamed hassan <mohamedisaaq0@gmail.com>
2022-07-14 19:40:10 +0100

    changes to the get route

--- controllers/api/moduleRoutes.js
+++ controllers/api/moduleRoutes.js
@@ -1,28 +1,12 @@
 const router = require('express').Router();
 const { Modules } = require('../../models');
 
-// get all modules
-router.get('/', async (req, res) => {
-  try {
-    const modules = await Modules.findAll({
-      include: [{ language }],
-      raw: true,
-    });
-    res.render('modules', { Modules });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
-  }
-});
-
 // get a specific module
 router.get('/:id', async (req, res) => {
   try {
-    const module = await Modules.findOne({
-      where: { id: req.params.id },
-      raw: true,
-    });
-    res.render('module', { module });
+    const module = await Modules.findbyPK(req.params.id);
+    const moduleData = module.get();
+    res.json(module);
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
