e586ddc7d26f03b06a3bfa6bc259c2c1798f0087
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-07-16 11:16:40 +0100

    Login forms

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -68,3 +68,12 @@ $(document).ready(function () {
     },
   });
 });
+
+switchers.forEach((item) => {
+  item.addEventListener('click', function () {
+    switchers.forEach((item) =>
+      item.parentElement.classList.remove('is-active')
+    );
+    this.parentElement.classList.add('is-active');
+  });
+});
