4bc02b0a6f728aeb04b7c35fbacc6e9fdb3f2648
mohamed hassan <mohamedisaaq0@gmail.com>
2022-07-19 18:33:56 +0100

    fixed seeds

--- models/index.js
+++ models/index.js
@@ -2,4 +2,16 @@ const Language = require('./language');
 const Module = require('./module');
 const User = require('./user');
 
+Language.hasMany(Module, {
+  foreignKey: 'language_id',
+});
+
+Module.belongsTo(Language, {
+  foreignKey: 'language_id',
+});
+
+User.hasMany(Language, {
+  foreignKey: 'user_id',
+});
+
 module.exports = { Language, Module, User };
