4c0944f82922407c784c11d49140e4cfa049bc2a
mohamed hassan <mohamedisaaq0@gmail.com>
2022-07-22 17:19:31 +0100

    module routes deleted

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const languageRoutes = require('./languageRoutes');
-const moduleRoutes = require('./moduleRoutes');
 
 router.use('/user', userRoutes);
 router.use('/language', languageRoutes);
-router.use('/module', moduleRoutes);
 
 module.exports = router;
