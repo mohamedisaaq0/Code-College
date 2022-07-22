5b7a1b01c8742f6572a46c0b3e8c8c1d5ecba3a5
mohamed hassan <mohamedisaaq0@gmail.com>
2022-07-22 17:12:52 +0100

    test comment

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,11 @@
 const sequelize = require('../config/connection');
-const seedModule = require('./module');
-// const seedLanguage = require('./language');
+// const seedModule = require('./module');
+const seedLanguage = require('./language');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  // await seedLanguage();
-  await seedModule();
+  await seedLanguage();
+  //await seedModule();
   process.exit(0);
 };
 
