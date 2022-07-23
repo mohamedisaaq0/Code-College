297f11a51061f4b03955a1bfa58777b9d43d0660
mohamed hassan <mohamedisaaq0@gmail.com>
2022-07-23 10:49:14 +0100

    last link fixed

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -39,7 +39,7 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/');
     } else {
       alert('User already exists, please try again!');
     }
